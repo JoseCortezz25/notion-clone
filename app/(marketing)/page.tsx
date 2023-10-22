d24bf78fcf8884b806e18967a88a907040efa3a6
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2023-10-22 18:53:19 -0500

    📦 NEW: added landing page

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Heading } from './_componentes/heading'
+import { Heroes } from './_componentes/heroes'
+import { Footer } from './_componentes/footer'
+
+const MarketingPage = () => {
+  return (
+    <main className="min-w-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+        <Footer />
+      </div>
+    </main>
+  )
+}
+
+export default MarketingPage
